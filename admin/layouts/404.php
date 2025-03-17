be98bc23dbe2af34b2370bb7ed7ccfbfa1082923
Tên của bạn <Email ca bn>
2025-03-18 00:33:13 +0700

    test

---
+++ admin/layouts/404.php
@@ -0,0 +1,11 @@
+<div class="container-fluid pt-4 px-4">
+    <div class="row vh-100 bg-lightrounded align-items-center justify-content-center mx-0 ">
+        <div class="col-md-6 text-center p-4">
+            <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
+            <h2 class="display-1 fw-bold">404</h2>
+            <h2 class="mb-4">Không tìm thấy trang</h2>
+            <h2>Rất tiếc,trang bạn tìm kiếm không tồn tại</h2>
+            <a href="btn btn-primary rounded-pill py-3 px-5 " href="">Quay lại trang chủ</a>
+        </div>
+    </div>
+</div>
---
+++ admin/layouts/sidebar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <aside class="sidebar">
+        <h2><i class="fas fa-user"></i>ADMIN</h2>
+        <ul>
+            <li><i class="fas fa-tachometer-alt"></i>Dashboard</li>
+            <li><i class="fas fa-shopping-cart"></i>Đơn hàng</li>
+            <li><i class="fas fa-list"></i> Danh mục</li>
+            <li><i class="fas fa-box"></i>Sản phẩm</li>
+            <li><i class="fas fa-newspaper"></i>Bài viết</li>
+            <li><i class="fas fa-chart-bar"></i>Thống kê</li>
+            <li><i class="fas fa-users"></i>Thành viên</li>
+            <li><i class="fas fa-comments"></i>Bình luận</li>
+        </ul>
+    </aside>
+</body>
+
+</html>
