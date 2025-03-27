b944bceaf9e1755c9139afc7d648258bcf9726c4
loan018 <loanptph52441@gmail.com>
2025-03-27 22:56:40 +0700

    loan

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php 
+
+// Require file Common
+require_once './commons/env.php'; // Khai báo biến môi trường
+require_once './commons/function.php'; // Hàm hỗ trợ
+
+// Require toàn bộ file Controllers
+require_once "./controllers/HomeController.php";
+
+
+// Require toàn bộ file Models
+
+
+// Route
+$act = $_GET['act'] ?? '/';
+
+
+match ($act) {
+    // Trang chủ
+    '/' => (new HomeController())->home(),
+};
